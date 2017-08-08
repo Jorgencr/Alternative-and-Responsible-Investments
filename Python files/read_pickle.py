bd4ac329d793029724f1eb2c1cae2bc7e2213af0
Jorgencr <Jorgen.c.r@gmail.com>
2017-08-08 23:47:31 +0100

    Add files via upload

---
+++ Python files/read_pickle.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  2 16:42:04 2017
+
+@author: Christopher
+"""
+import pandas as pd
+
+totret = pd.read_pickle('totret')
+icm = pd.read_pickle('icm')
+dividends = pd.read_pickle('dividends')
+dividends2 = pd.read_pickle('dividends2')
+investmentuniverse = pd.read_pickle('investmentuniverse')
+rf = pd.read_pickle('rf')
+df = pd.read_pickle('df')
+fama = pd.read_pickle('fama')
+DoDportfolios = pd.read_pickle('DoDportfolios')
